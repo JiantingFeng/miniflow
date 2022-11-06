a01acfe70c7bfb0ae546fd1b54276093cd2bab11
JiantingFeng <gianting01@gmail.com>
2022-11-06 19:40:43 +0800

    feat(tensor.py): add several numpy-alike classmethods to create Tensor

--- src/miniflow/tensor.py
+++ src/miniflow/tensor.py
@@ -18,10 +18,45 @@ class Tensor:
     requires_grad : bool, default=False
     '''
 
-    def __init__(self, data, requires_grad=False):
+    def __init__(self, data, requires_grad=False) -> None:
         if not isinstance(data, np.ndarray):
             raise TypeError('data must be a numpy.ndarray')
         self.data = data
         self.requires_grad = requires_grad
         # gradient tensor (Optional)
-        self.rgad: Optional[Tensor] = None
+        self.grad: Optional[Tensor] = None
+
+    def __repr__(self) -> str:
+        return f"<Tensor: {self.data}, requires_grad={self.requires_grad}>"
+
+    # Properties (read-only)
+
+    @property
+    def shape(self) -> tuple:
+        return self.data.shape
+
+    @property
+    def dtype(self) -> np.dtype:
+        return self.data.dtype
+
+    # Classmethods: wrap some common numpy.ndarray methods
+    @classmethod
+    def zeros(cls, *shape, **kwargs):
+        return cls(np.zeros(shape, dtype=np.float32), **kwargs)
+
+    @classmethod
+    def ones(cls, *shape, **kwargs):
+        return cls(np.ones(shape, dtype=np.float32), **kwargs)
+
+    @classmethod
+    def randn(cls, *shape, **kwargs):
+        # shape of randn donnot need to be wrapped by tuple
+        return cls(np.random.randn(*shape), **kwargs)
+
+    @classmethod
+    def uniform(cls, *shape, **kwargs):
+        return cls(np.random.uniform(*shape)*2-1, **kwargs)
+
+    @classmethod
+    def eye(cls, n, **kwargs):
+        return cls(np.eye(n, dtype=np.float32), **kwargs)
